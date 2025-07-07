efb93d6a2f39dee74bf0f5d555a308b37e1bf0d1
Mariia D <mariia.demidova0304@gmail.com>
2025-07-06 22:53:45 -0400

    changed object, make a function to render a news task in a li element

--- app.js
+++ app.js
@@ -9,10 +9,18 @@ const taskList = document.getElementById(`task-list`);
 addTaskButton.addEventListener(`click`, () => {
     console.log(`clicked`)
     let newTask = {
-        taskName: taskNameInput.value,
-        taskCategory: categoryInput.value,
-        taskDeadline: deadlineInput.value,
-        taskStatus: statusSelect.value,
+        name: taskNameInput.value,
+        category: categoryInput.value,
+        deadline: deadlineInput.value,
+        status: statusSelect.value,
     }
     console.log(newTask);
+    renderTask(newTask);
 })
+
+//make a function to render a task li
+function renderTask(task){
+    let newTaskItem = document.createElement(`li`);
+    newTaskItem.innerHTML = `<span>${task.name}</span><span>${task.category}</span><span>${task.deadline}</span><span>${task.status}</span><button id="delete-task">Delete</button>`;
+    taskList.appendChild(newTaskItem);
+}
